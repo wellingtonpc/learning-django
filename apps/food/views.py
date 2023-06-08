1ee468ee1365e63135081533203049b16ece2f62
Wellington Costa <well_pcosta@hotmail.com>
2023-06-08 20:21:27 -0300

    add template and uses it to show view Item

--- apps/food/views.py
+++ apps/food/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
+from django.template import loader
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World!')
+    item_list = Item.objects.all()
+    template = loader.get_template('food/index.html')
+    context = {
+        'item_list': item_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def item(request):
     return HttpResponse('This is an item view')
