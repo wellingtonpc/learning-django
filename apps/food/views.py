031eed0f3e439102b66832fcdef87500b47294da
Wellington Costa <well_pcosta@hotmail.com>
2023-06-08 20:24:07 -0300

    use render in return of view index

--- apps/food/views.py
+++ apps/food/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
-from django.template import loader
-
 
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    template = loader.get_template('food/index.html')
     context = {
         'item_list': item_list,
     }
-    return HttpResponse(template.render(context, request))
+
+    return render(request, 'food/index.html', context)
 
 def item(request):
     return HttpResponse('This is an item view')
