02bc40faa3236ffbef3a5045ddb335489bdab8be
Wellington Costa <well_pcosta@hotmail.com>
2023-06-09 16:04:51 -0300

    add delete item to detail

--- apps/food/urls.py
+++ apps/food/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('detail/<int:item_id>', views.detail, name='detail'),
     path('add-item', views.add_item, name='add_item'),
     path('update-item/<int:item_id>', views.update_item, name='update_item'),
+    path('delete-item/<int:item_id>', views.delete_item, name='delete_item'),
 ]
--- apps/food/views.py
+++ apps/food/views.py
@@ -37,3 +37,12 @@ def update_item(request, item_id):
         return redirect('food:index')
 
     return render(request, 'food/item-form.html', {'form': form})
+
+def delete_item(request, item_id):
+    item = Item.objects.get(id=item_id)
+    
+    if request.method == 'POST':
+        item.delete()
+        return redirect('food:index')
+    
+    return render(request, 'food/item-delete.html', {'item': item})
