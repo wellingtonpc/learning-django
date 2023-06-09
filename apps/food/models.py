4585a490aea448f633a6c1717ee163b54c533161
Wellington Costa <well_pcosta@hotmail.com>
2023-06-09 00:18:10 -0300

    add image_url to Item model

--- apps/food/models.py
+++ apps/food/models.py
@@ -6,6 +6,10 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.FloatField(default=0)
+    image_url = models.CharField(
+        max_length=500, 
+        default='https://upload.wikimedia.org/wikipedia/commons/6/62/Image_unavailable.png'
+    )
 
     def __str__(self):
         return f'Name {self.item_name}, $ {self.item_price:.2f}'
