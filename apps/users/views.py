39d8c4d80e5359274aaedd8f0d1b96c5c6ea8429
Wellington Costa <well_pcosta@hotmail.com>
2023-06-10 11:01:02 -0300

    add email to user register form

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages 
+from .forms import RegisterForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             user_name = form.cleaned_data.get('username')
             messages.success(request, f'Welcome {user_name}, your account is created!')
             return redirect('food:index')
     else:
-        form = UserCreationForm()        
+        form = RegisterForm()        
 
     return render(request, 'users/register.html', {'form': form})
