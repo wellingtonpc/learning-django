0878f52ae47a9745b3b9007b9a7f5ccc5b103063
Wellington Costa <well_pcosta@hotmail.com>
2023-06-09 22:30:50 -0300

    add message for success signup

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib import messages 
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_name = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome {user_name}, your account is created!')
+            return redirect('food:index')
+    else:
+        form = UserCreationForm()        
 
     return render(request, 'users/register.html', {'form': form})
