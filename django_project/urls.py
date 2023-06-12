9bb36d3a3cb589f3268309c7ce5526fb098738c0
Wellington Costa <well_pcosta@hotmail.com>
2023-06-12 19:31:11 -0300

    add login and logout views and templates

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,11 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from apps.users import views as users_views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include('apps.food.urls')),
     path('users/', users_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='users/login.html'
+    ), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='users/logout.html'
+    ), name='logout'),
 
 ]
