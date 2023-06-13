98002fbf5ac73c25c0de4381a2d49504daf67c1f
Wellington Costa <well_pcosta@hotmail.com>
2023-06-12 22:12:05 -0300

    extends User to Profile

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(User):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(
+        default='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+        upload_to='profile_pictures'
+    )
+    location = models.CharField(max_length=100)
